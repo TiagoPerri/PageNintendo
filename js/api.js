d39fb4c392c2d31cad55e89b2f2d91e236a90874
LeonardoCGoulart <joedarkalone@hotmail.com>
2021-04-07 01:13:55 -0300

    testes para a implementação de contato com a API

--- js/api.js
+++ js/api.js
@@ -1,9 +1,13 @@
+
+
+
 var button = document.querySelector('button'); // recupera botao
 var lista = document.querySelector('ul'); // recupera ul
 button.addEventListener('click', function(){
     var li = document.createElement(li); // cria um li para colocar no ul
     var query = document.querySelector('input').value; // recupera texto do input
-    li.innerHTML = '<li>'+query+'</li>'; // aqui adiciona no LI a palavra que ta no input (query)
-    lista.appendChild(li);	// faz aparecer na tela
-    document.querySelector('input').value = ''; // limpar barra de pesquisa
-})
+    axios.get('https://superheroapi.com/api/3708772112566731/70')
+    .then(function(res){
+        console.log(res);
+    });
+});
