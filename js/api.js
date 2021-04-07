7b79151c3cfb93797a4bd2379099eed25918921f
LeonardoCGoulart <joedarkalone@hotmail.com>
2021-04-07 19:19:30 -0300

    adicionado funcionalidade de conexão com a API do projeto

--- js/api.js
+++ js/api.js
@@ -1,13 +1,38 @@
+const lista = document.querySelector('ul'); 
+let personagens = {};
+let results = [];
 
+const getPersonagens = async () => { // função assíncrona
+    let query = document.querySelector('input').value; // recupera texto do input
+    try {
+        const res = await fetch(`https://www.superheroapi.com/api.php/3708772112566731/search/${query}`); // capta os dados da API de acordo com a query
+        personagens = await res.json();
+        results = personagens.results;
+        mostrarPersonagens(results);
+    } catch (falha) {
+        console.error(falha);
+    }
+};
 
-
+const mostrarPersonagens = (characters) => {
+    const htmlString = characters
+        .map((character) => { // array 
+            return `       
+            <li>
+                    <h1>Nome: ${character.name}</h1></a>
+                    <h2>Editora: ${character.biography.publisher}</h2></a>
+                    <img class="containerHero image" src="${character.image.url}"></img>
+                    <p>inteligência: ${character.powerstats.intelligence}</p>
+                    <p>força: ${character.powerstats.strength}</p>
+                    <p>poder: ${character.powerstats.power}</p>
+                    <p>durabilidade: ${character.powerstats.durability}</p> 
+                
+           </li>
+      
+        `
+        })
+        .join('');
+    lista.innerHTML = htmlString; // adiciona o conteudo
+};
 var button = document.querySelector('button'); // recupera botao
-var lista = document.querySelector('ul'); // recupera ul
-button.addEventListener('click', function(){
-    var li = document.createElement(li); // cria um li para colocar no ul
-    var query = document.querySelector('input').value; // recupera texto do input
-    axios.get('https://superheroapi.com/api/3708772112566731/70')
-    .then(function(res){
-        console.log(res);
-    });
-});
+button.addEventListener("click", getPersonagens); // faz carregar a pesquisa ao clicar, chamando a função
