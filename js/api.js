977b659e36823a0b35c4732a30aaa43103a42565
TiagoPerri <tiagocnsr@gmail.com>
2021-04-07 02:01:45 -0300

    add script axios

--- js/api.js
+++ js/api.js
@@ -1,13 +1,9 @@
-
-
-
 var button = document.querySelector('button'); // recupera botao
 var lista = document.querySelector('ul'); // recupera ul
 button.addEventListener('click', function(){
     var li = document.createElement(li); // cria um li para colocar no ul
     var query = document.querySelector('input').value; // recupera texto do input
-    axios.get('https://superheroapi.com/api/3708772112566731/70')
-    .then(function(res){
+    axios.get('https://superheroapi.com/api/3708772112566731/70').then(function(res){
         console.log(res);
     });
 });
