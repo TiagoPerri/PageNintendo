b9cea54776a36fbbe2497fbb7bb1f6fcae32be18
TiagoPerri <tiagocnsr@gmail.com>
2021-04-11 02:10:00 -0300

    Pequeno ajuste localStorage.js

--- js/localStorage.js
+++ js/localStorage.js
@@ -7,8 +7,8 @@ if(localStorage.password_login){
 }
 
 var salvarLogin = function(){
-    var email = document.getElementById('email_login').value;
-    var senha = document.getElementById('password_login').value;
+    var email_login = document.getElementById('email_login').value;
+    var password_login = document.getElementById('password_login').value;
     localStorage.setItem('email_login', email_login);
     localStorage.setItem('senha_login', password_login);
 }
