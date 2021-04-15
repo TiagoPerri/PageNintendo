d0aad4597e7f4a8332e3f01f10b32d64ec68c742
LeonardoCGoulart <joedarkalone@hotmail.com>
2021-04-15 14:42:29 -0300

    deixando o sistema funcional, conectado com ReqRes e com validação de campos

--- js/api.js
+++ js/api.js
@@ -4,16 +4,29 @@ let results = [];
 
 const getPersonagens = async () => { // função assíncrona
     let query = document.querySelector('input').value; // recupera texto do input
-    try {
-        const res = await fetch(`https://www.superheroapi.com/api.php/3708772112566731/search/${query}`); // capta os dados da API de acordo com a query
-        personagens = await res.json();
-        results = personagens.results;
-        mostrarPersonagens(results);
-    } catch (falha) {
-        console.error(falha);
-    }
-};
 
+    if(!localStorage.token){ // nao existe token na sessão
+        console.log('voce não esta logado');
+        var img = document.createElement("img");
+        img.src="assets/api_fail.png";
+        img.alt="error login";
+        img.width=170;
+        img.height=90;
+        document.getElementById('div-hero').appendChild(img);
+        
+        return false;
+    }
+        try {
+            const res = await fetch(`https://www.superheroapi.com/api.php/3708772112566731/search/${query}`); // capta os dados da API de acordo com a query
+            personagens = await res.json();
+            results = personagens.results;
+            mostrarPersonagens(results);
+        } catch (falha) {
+            console.error(falha);
+        }
+        };
+    
+    
 const mostrarPersonagens = (characters) => {
     const htmlString = characters
         .map((character) => { // array 
@@ -33,5 +46,8 @@ const mostrarPersonagens = (characters) => {
         .join('');
     lista.innerHTML = htmlString; // adiciona o conteudo
 };
+
+
+
 var button = document.querySelector('button'); // recupera botao
 button.addEventListener("click", getPersonagens); // faz carregar a pesquisa ao clicar, chamando a função
--- js/reqRes.js
+++ js/reqRes.js
@@ -1,62 +1,45 @@
-document.addEventListener('DOMContentLoaded', function(){
-	acessoInvalido();
-
-	var entrarLogin = document.getElementById("login");
-	var btnSubmit = document.getElementsById("BtnSubmit");
-	var entrarCadastro = document.getElementById("cadastro");
-
-	entrarCadastro.addEventListener("submit", function (event){
-		event.preventDefault();
-		requisicaoLogar();
-	});
-
-	entrarLogin.addEventListener("submit", function (event){
-		event.preventDefault();
+var entrarLogin = document.getElementById("BtnSubmit"); 
+var entrarCadastro = document.getElementById("BtnSubmit_cadastro");
+let serverTalk = ""; // mensagens de erro do servidor vão ser armazenados aqui
+if (entrarLogin != null){ // está no modo de Login
+	entrarLogin.addEventListener("click", function (event){
 		requisicaoLogin();
 	});
-});
+}
+
+if (entrarCadastro != null){ // está no modo de Registro
+    entrarCadastro.addEventListener("click", function (event){
+		requisicaoCadastro();
+	});
+}
+
 
 function requisicaoLogin(){
 	var campoUser = document.getElementById("email_login");
 	var campoSenha = document.getElementById("password_login");
-	var errorMessage = document.querySelector("");
 
-	if(campoUser.value != null && campoSenha.value != null){
-		requisicaoLogin(campoUser.value, campoSenha.value, function (response){
-			if(response.error){
-				errorMessage.classList.add("");
-				errorMessage.innerHTML = response.error;
-				funcError();
-			}else{
-				localStorage.setItem("loginToken", response.token);
+	if(fazerValidacao(campoUser,campoSenha,"onLogin") == true){
+		doLogin(campoUser.value, campoSenha.value, function (response){
+				localStorage.setItem("token", response.token);
 				irPageAPI();
-			}
 		});
 	}
 }
 
-function requisicaoLogar(){
+function requisicaoCadastro(){
 	var campoUser = document.getElementById("email_login");
 	var campoSenha = document.getElementById("password_login");
-	var registerBoxError = document.querySelector("");
 
-	if(campoUser.value != null && campoSenha.value != null){
-		requisicaoCadastro(campoUser.value, campoSenha.value, function (response){
-			if(response.error){
-				registerBoxError.classList.add("");
-				registerBoxError.innerHTML = response.error;
-				funcError();
-			}else{
-				localStorage.setItem("loginToken",response.token);
-				irPageAPI();
-			}
+	if(fazerValidacao(campoUser,campoSenha,"onRegister") == true){
+		doRegister(campoUser.value, campoSenha.value, function (response){
+			localStorage.setItem("token", response.token);
+			irPageAPI();
 		});
 	}
 }
 
-function requisicaoLogin(email, password, callback){
+function doLogin(email, password, callback){
 	var requisicao = new XMLHttpRequest();
-
 	requisicao.open("POST", "https://reqres.in/api/login", true);
 	requisicao.setRequestHeader("Content-Type", "application/json; charset=utf-8")
 	requisicao.onreadystatechange = function () {
@@ -66,13 +49,13 @@ function requisicaoLogin(email, password, callback){
 		}
 		callback(JSON.parse(requisicao.responseText));
 	};
-	requisicao.send(JSON.stringify({
+	requisicao.send(JSON.stringify({ // converte valores em javascript para uma String JSON
 		email: email,
 		password: password
 	}));
 }
 
-function requisicaoCadastro(email, password, callback){
+function doRegister(email, password, callback){
     var requisicao = new XMLHttpRequest();
 
     requisicao.open("POST", "https://reqres.in/api/register", true);
@@ -84,7 +67,7 @@ function requisicaoCadastro(email, password, callback){
         }
         callback(JSON.parse(requisicao.responseText));
     };
-    requisicao.send(JSON.stringify({
+    requisicao.send(JSON.stringify({ // converte valores em javascript para uma String JSON
         email: email,
         password: password
     }));
@@ -94,22 +77,76 @@ function irPageAPI(){
 	window.location.href = "api.html"
 }
 
-function acessoInvalido(){
-	var data = new URLSearchParams(window.location.search);
-	var acessoInvalido = data.get("acessoInvalido");
-	var errorMessage = document.querySelector("");
-
-	if(acessoInvalido != null){
-		errorMessage.classList.add("");
-		errorMessage.innerHTML = "Login não realizado";
-		ativarModal();
-	}
-}
-
-function logar(){
-	let token = localStorage.getItem("loginToken");
-
-	if(token != null){
+function userLogado(){
+	if(localStorage.token){ 
 		return true;
 	}
+    return false;
+}
+
+function fazerValidacao(email,password,type){
+    
+    // credenciais que o ReqRes tem disponivel e aceita para o login
+    var apiOnLoginEmail = "eve.holt@reqres.in";
+    var apiOnLoginPassword = "cityslicka";
+
+    // credenciais que o ReqRes tem disponivel e aceita para o registro
+    var apiOnRegisterEmail = "eve.holt@reqres.in";
+    var apiOnRegisterPassword = "pistol";
+
+    serverTalk = ""; // limpa
+    if (userLogado() == true){
+        serverTalk = serverTalk + "voce já está logado";
+        alert(serverTalk);
+        return false;       
+    }
+    if(email.value == null || email.value == "") {
+        serverTalk = serverTalk + "email não pode ficar vazio";
+        alert(serverTalk);
+        return false;
+    }
+    if(password.value == null || password.value == "") {
+        serverTalk = serverTalk + "senha não pode ficar vazio";
+        alert(serverTalk);
+        return false;
+    }
+    if(email.value.length <= 3) {
+        serverTalk = serverTalk + "email muito curto";
+        alert(serverTalk);
+        return false;
+    }
+    if(password.value.length <= 3) {
+        serverTalk = serverTalk + "senha muito curta";
+        alert(serverTalk);
+        return false;
+    }
+    if (type == "onLogin") { // tipo de validação = Login
+        if(email.value != apiOnLoginEmail){
+            serverTalk = serverTalk + "email não cadastrado (não existe no reqres.in) ";
+            serverTalk = serverTalk + "use: "+apiOnLoginEmail+" ";
+            alert(serverTalk);
+            return false;
+        }
+        if(password.value != apiOnLoginPassword){
+            serverTalk = serverTalk + "senha invalida (não existe no reqres.in) ";
+            serverTalk = serverTalk + "use: "+apiOnLoginPassword+" ";
+            alert(serverTalk);
+            return false;
+        }
+    }   
+    if (type == "onRegister") { // tipo de validação = Register
+        if(email.value != apiOnRegisterEmail){
+            serverTalk = serverTalk + "voce não pode cadastrar esse email (não existe no reqres.in) ";
+            serverTalk = serverTalk + "use: "+apiOnRegisterEmail+" ";
+            alert(serverTalk);
+            return false;
+        }
+        if(password.value != apiOnRegisterPassword){
+            serverTalk = serverTalk + "voce não pode cadastrar essa senha (não existe no reqres.in) ";
+            serverTalk = serverTalk + "use: "+apiOnRegisterPassword+" ";
+            alert(serverTalk);
+            return false;
+        }
+    }   
+    return true;
 }
